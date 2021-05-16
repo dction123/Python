9e973046b6acaf11557210969ac870f2f0648183
dction123 <60395656+dction123@users.noreply.github.com>
2021-05-16 10:33:43 +0800

    Add files via upload
    python基础

---
+++ python基础/02.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding:utf8
+# ------------------------
+__time__ = '2021/4/13 21:26'
+__author__ = 'Liang'
+# ------------------------
+
+# name = input("请输入一个数 \n \n按回车确认")  # 用户输入
+# print(name)
+#
+#
+# import sys; x = 'runoob'; sys.stdout.write(x+'\n')  # 多行代码一行显示
+
+# x = '10'
+# y = '100'
+# print('-----换行输出------')
+# print(x)
+# print(y)
+# print('-----不换行输出------')
+# print(x,end="")
+# print(y,end="")
+# import sys
+# print('命令行参数为：')
+# for i in sys.argv:
+#     print(i)
+# print('\n python 路径为',sys.path)
+while True:
+    print('12')
+
+
+
+# from sys import argv, path  # 引入一个模块中的多个函数
---
+++ python基础/05.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding:utf8
+# ------------------------
+__time__ = '2021/5/15 23:51'
+__author__ = 'Liang'
+# ------------------------
+'''
+变量参数
+*变量 以元组形式
+**变量 以字典形式
+'''
+
+
+# def demo(name, age):
+#     print(name)
+#     print(age)
+#
+#
+# demo('张三', 22)
+
+# def demo2(*par, age, gender):
+#     print(type(par))
+#     for p in par:
+#         print(p + str(age) + gender)
+#
+
+# demo2('张三', '李四', '王五', age=10, gender='男')
+# def demo3(*par):
+#     print(type(par))
+#     for p in par:
+#         print(p)
+#
+#
+# demo3(('张三', '李四', '王五'))
+
+
+# def demo4(num, **par):
+#     print(par)
+#
+#
+# demo4(1, key='小小', age=10)
+
